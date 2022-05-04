3b642569939d62a2f9fcce751b69f73ffcd01532
Xingjian Hao <haoxj0122@tamu.edu>
2022-05-03 23:57:44 -0500

    Modified script file

--- histgram_gen.py
+++ histgram_gen.py
@@ -1,16 +1,18 @@
+# Python Scrip to plot the histogram
 import numpy as np 
 import matplotlib.pyplot as plt
 
 file_t = open("count1", "r")
-file_s = open("count2.txt", "r")
+file_s = open("count2", "r")
 
+# data input from files
 times, counts = [], []
 for line in file_t:
     time = line[0:2]
     if (time[0] == '0'):
-        time = time[1]
-
-    count = line[line.find(' ') + 1:len(line)-1]
+         time = time[1]
+        
+    count = line[line.find(' ')+1:len(line)-1]
     times.append(int(time))
     counts.append(int(count))
 
@@ -18,18 +20,16 @@ times2, counts2 = [], []
 for line in file_s:
     time = line[0:2]
     if (time[0] == '0'):
-        time = time[1]
-
-    count = line[line.find(' ') + 1:len(line)-1]
+         time = time[1]
+        
+    count = line[line.find(' ')+1:len(line)-1]
     times2.append(int(time))
     counts2.append(int(count))
 
+# generate histograms
 fig, ax = plt.subplots(2)
-
 ax[0].bar(times, counts)
 ax[0].set_title("Tweets Count vs Hour")
-
-
 ax[1].bar(times2, counts2)
 ax[1].set_title("Sleep Tweets Count vs Hour")
 
